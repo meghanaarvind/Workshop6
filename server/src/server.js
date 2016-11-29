7796e928a12dbf608bf6c9da423a13ce672c79b6
Meghana Arvind <meghana.arvind@gmail.com>
2016-11-29 16:25:30 -0500

    express

---
+++ server/src/server.js
@@ -0,0 +1,38 @@
+//imports the express Node module
+var express = require('express')
+//creates an express server
+var app = express();
+
+//import reverseString
+var util = require('./util');
+var reverseString = util.reverseString;
+
+var bodyParser = require('body-parser');
+
+//defines what happens when it receives the 'GET /' request
+app.get('/', function (req, res) {
+	res.send('Hello World!');
+});
+
+//starts the server on port 3000:
+app.listen(3000, function () {
+	console.log('Example app listening on port 3000!');
+});
+
+
+app.use(bodyParser.text());
+
+// Handle POST /reverse [data]
+app.post('/reverse', function (req, res) {
+	// If the request came with text, then the text() middleware handled it
+	// and made `req.body` a string.
+	// Check that req.body is a string.
+	if (typeof(req.body) === 'string') {
+		var reversed = reverseString(req.body);
+		res.send(reversed);
+	} 
+	else {
+		// POST did not contain a string. Send an error code back!
+		res.status(400).end()
+	}
+});
