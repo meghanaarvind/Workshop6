3767761420d79f8d71069dd86e68630e014b2a57
Meghana Arvind <meghana.arvind@gmail.com>
2016-11-30 15:28:09 -0500

    almostDone

--- client/app/database.js
+++ client/app/database.js
@@ -204,10 +204,14 @@ export class ResetDatabase extends React.Component {
   render() {
     return (
       <button className="btn btn-default" type="button" onClick={() => {
-        resetDatabase();
+      var xhr = new XMLHttpRequest();
+      xhr.open('POST', '/resetdb');
+      xhr.addEventListener('load', function() {
         window.alert("Database reset! Refreshing the page now...");
         document.location.reload(false);
-      }}>Reset Mock DB</button>
+      });
+      xhr.send();
+    }}>Reset Mock DB</button>
     );
   }
 }
